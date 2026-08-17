02d8468998d4b60e4a1d53795c56a3ec256976fe
Maria B <blokhinamariyayu@gmail.com>
2026-08-17 15:32:40 +0000

    format fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,15 +3,15 @@ import globals from 'globals';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  {
-    files: ['**/*.{js,mjs,cjs}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.node },
-    rules: {
-      'no-console': 'warn',
-      'prefer-const': 'error',
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        languageOptions: { globals: globals.node },
+        rules: {
+            'no-console': 'warn',
+            'prefer-const': 'error',
+        },
     },
-  },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+    { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
 ]);
